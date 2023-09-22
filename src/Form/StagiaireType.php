60ef4d80d102beb018bc61040b7f76c670132a1c
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-22 15:35:04 +0200

    using query builder

--- src/Form/StagiaireType.php
+++ src/Form/StagiaireType.php
@@ -2,11 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Session;
 use App\Entity\Stagiaire;
+use App\Repository\SessionRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -17,11 +20,18 @@ class StagiaireType extends AbstractType
         $builder
             ->add('nom' , TextType::class)
             ->add('prenom', TextType::class)
-            ->add('dateDeNaissance0', DateType::class)
+            ->add('dateDeNaissance', DateType::class)
             ->add('email', TextType::class)
-            ->add('sessions', SessionType::class, [
-                "class" => StagiaireType::class,
+            ->add('sessions', EntityType::class, [ //Must find a way to display only sessions that have not ended yet
+                "class" => Session::class,
                 "choice_label" => "titre",
+                "multiple" => true,
+                'query_builder' => function (SessionRepository $er) { //Query builder allows us to do custom queries to matche specific conditions
+                    return $er->createQueryBuilder('s') //Here we don't want to display sessions that are end meaning we select only sesions where endDate are at least tomorrow
+                        ->where('s.dateSessionFin > :currentDate')
+                        ->setParameter('currentDate', new \DateTime())
+                        ->orderBy('s.dateSessionFin', 'ASC');
+                },
             ])
             ->add('valider', SubmitType::class)
         ;
