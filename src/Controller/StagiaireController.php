f3b9dfb8f10f52f47ff4c154eee38937f62975cd
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-22 15:58:38 +0200

    imported need class and persist/flush to add data

--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Stagiaire;
 use App\Form\StagiaireType;
 use App\Repository\StagiaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,12 +23,20 @@ class StagiaireController extends AbstractController
     }
 
     #[Route('/stagiaire/new', name: 'newStagiaire')]
-    public function new(Request $request): Response
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         // creates a task object and initializes some data for this example
         $stagiaire = new Stagiaire();
         $form = $this->createForm(StagiaireType::class , $stagiaire );
-
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+            {
+                $stagiaire = $form->getData();
+                $entityManager->persist($stagiaire); //traditional prepare / execute in SQL
+                $entityManager->flush();
+                return $this->redirectToRoute('globalStagiaire'); //redirect to list stagiaires if everything is ok
+            }
+        
         return $this->render("stagiaire/new.html.twig", ['formNewStagiaire' => $form]);
     }
 
