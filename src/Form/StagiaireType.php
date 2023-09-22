1a9cb3e0aea6d5fe9052d3d7fb5d3a734abc9f13
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-22 14:47:37 +0200

    creating form for stagiaire add

---
+++ src/Form/StagiaireType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Stagiaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class StagiaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom' , TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('dateDeNaissance0', DateType::class)
+            ->add('email', TextType::class)
+            ->add('sessions', SessionType::class, [
+                "class" => StagiaireType::class,
+                "choice_label" => "titre",
+            ])
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stagiaire::class,
+        ]);
+    }
+}
