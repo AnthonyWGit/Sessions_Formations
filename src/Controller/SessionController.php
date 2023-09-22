7841e3c96d0b7d384538089ff2401f50fb646217
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-22 09:42:26 +0200

    test to count number of remaining places in ctrl

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -15,17 +15,23 @@ class SessionController extends AbstractController
     #[Route('/sessions', name: 'globalSession')]
     public function index(SessionRepository $sessionRepository): Response
     {
+
         $sessions = $sessionRepository->findBy([], ["titre" => "ASC"]);
 
-        foreach ($sessions as $session)
-        {
-            foreach ($session->getStagiaires() as $stagiaire)
-            {
-                $arrayCount[$session->getId()] = $session->getPlaces() - 1;
-            }
-        }
+        // foreach ($sessions as $session) Trying to do counting via controller  i get good arrays but i dunno how i can use it to say give index 0 to session id 1 etc.
+        // {
+        //     $placesRestantes = 0;
+
+        //     foreach ($session->getStagiaires() as $stagiaire)
+        //     {
+        //         $placesRestantes = $placesRestantes + 1;
+        //     }
+        //     $arrayCount[$session->getId()] = $session->getPlaces() - $placesRestantes;
+        // }
+
         return $this->render('session/index.html.twig', [
             'sessions' => $sessions,
+            'arrayCount' => $arrayCount,
         ]);
     }
 
