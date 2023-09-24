8e0f19b0e8d58cca3f9024094090efac9e77e788
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-24 12:21:17 +0200

    need calendar 4 date & multiple choices stagiaires

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -3,22 +3,38 @@
 namespace App\Form;
 
 use App\Entity\Session;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use App\Entity\Stagiaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('dateSessionDebut')
-            ->add('dateSessionFin')
-            ->add('places')
-            ->add('stagiaires')
-            ->add('formation')
-            ->add('formateur')
+            ->add('titre' , TextType::class)
+            ->add('dateSessionDebut', DateType::class)
+            ->add('dateSessionFin', DateType::class)
+            ->add('places', NumberType::class)
+            ->add('stagiaires', EntityType::class, [
+                'class' => Stagiaire::class,
+                'multiple' => true,
+            ])
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class
+            ])
+            ->add('formateur', EntityType::class , [
+                'class' => Formateur::class,
+            ])
+            ->add('valider', SubmitType::class)
         ;
     }
 
