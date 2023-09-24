892ca1ccc9b46f6703507ec372fca782ebf5ad24
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-24 12:20:22 +0200

    controller + template add a formateur

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Formateur;
+use App\Form\FormateurType;
 use App\Repository\FormateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +20,31 @@ class FormateurController extends AbstractController
         return $this->render('formateur/index.html.twig', [
             'formateurs' => $formateurs,
         ]);
+    }
+
+    #[Route('/formateur/new', name: 'newFormateur')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // creates a task object and initializes some data for this example
+        $formateur = new Formateur();
+        $form = $this->createForm(FormateurType::class, $formateur );
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+            {
+                $formateur = $form->getData();
+                $entityManager->persist($formateur); //traditional prepare / execute in SQL
+                $entityManager->flush();
+
+                $this->addFlash // need to be logged as user to see the flash messages build-in Symfony
+                (
+                    'notice',
+                    'Your changes were saved!'
+                );
+
+                return $this->redirectToRoute('globalFormateur'); //redirect to list stagiaires if everything is ok
+            }
+        
+        return $this->render("formateur/new.html.twig", ['formNewFormateur' => $form]);
     }
 
     #[Route('/formateur/{id}', name: 'detailFormateur')]
