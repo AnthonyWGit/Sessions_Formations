bf0f23f3576fafa697e1919ae28768cfd199edba
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-25 10:35:28 +0200

    form adding a module

--- src/Form/ModuleSessionType.php
+++ src/Form/ModuleSessionType.php
@@ -2,12 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Session;
 use App\Entity\Categorie;
+use App\Entity\Programme;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ModuleSessionType extends AbstractType
