98f53cf0b661715d8899e64f5fddd0fb0ed444ad
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-09-28 16:53:05 +0200

    modify credentials route

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\CredentialsFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -22,6 +26,42 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/modifyCredentials/{id}', name: 'modifyCredentials')]
+    public function mod(User $user, Request $request, EntityManagerInterface $entityManager) : Response
+    { 
+        if(!$this->getUser()){
+
+            return $this->redirectToRoute('app_login');
+
+        }
+        $form = $this->createForm(CredentialsFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Les informations de votre compte ont bien été modifiées'
+            );
+
+            return $this->redirectToRoute('globalSession');
+        }
+        return $this->render('security/credentials.html.twig', [
+
+            'form' => $form
+
+        ]);
+
+    }
+
+    #[Route(path: '/profile', name: 'app_profile')]
+    public function profile()
+    { 
+        return $this->render('security/profile.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
