2c3f7b2fb2bbad5affc3af0a2d20525459b7f5da
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-02 10:10:49 +0200

    adding a contraint endate > begindate

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -9,9 +9,11 @@ use App\Entity\Stagiaire;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -29,7 +31,13 @@ class SessionType extends AbstractType
             ])
             ->add('dateSessionFin', DateType::class, [
                 'widget' =>'single_text',
-
+                'constraints' => [
+                    new NotBlank(), 
+                    new GreaterThan([
+                        'message' => 'La date de fin doit être supérieure à celle de début',
+                        'propertyPath' => 'parent.all[dateSessionDebut].data'
+                    ])
+                ]
             ])
             ->add('places', NumberType::class, [
 
