b6bbea414ba2a5a7b07bd41209024af1c0a41cd8
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-02 12:01:37 +0200

    trying do a function to swiitch to table view

--- public/js/switch.js
+++ public/js/switch.js
@@ -2,10 +2,16 @@ $(document).ready(function ()
 {
     function toggleDisplay()
     {
+        //First we hide initial display
         $(cardBoard).hide()
-    }
+        //creating the table 
 
+    }
+    
+    $(tableBoard).hide()
+    
     const switchbtn = document.querySelector("#switchBtn")
     const cardBoard = document.querySelector(".cardBoard")
+    const tableBoard = document.querySelector("#tableBoard")
     switchbtn.addEventListener("click", toggleDisplay)
 })
