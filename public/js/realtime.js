65e8891c8bb6e432672964d263ae63c9946252f4
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-10-03 16:34:48 +0200

    checking if cards are on to not mess w table

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -1,51 +1,58 @@
 $(document).ready(function () {
-    var debounceTimer;
+    //select carboard and children
+    var cardBoard = $('.cardBoard');
+    if ($(cardBoard).is(':visible'))
+    {
+        var debounceTimer;
 
-    $('#searchInput').keyup(function () {
-        var searchTerm = $(this).val();
-        var route = $(this).data('route'); // Get the route path from the data attribute
+        $('#searchInput').keyup(function () {
+            var searchTerm = $(this).val();
+            var route = $(this).data('route'); // Get the route path from the data attribute
 
-        // Select the cardBoard and its children
-        var cardBoard = $('.cardBoard');
 
-        // Clear the debounce timer if it's already running
-        clearTimeout(debounceTimer);
+            // Clear the debounce timer if it's already running
+            clearTimeout(debounceTimer);
 
-        // Set a new debounce timer to delay the AJAX request
-        debounceTimer = setTimeout(function () {
-            $.ajax({
-                type: 'GET',
-                url: route,
-                data: { searchTerm: searchTerm },
-                success: function (data) {
-                    // Clear the card container
-                    $('#filteredCards').empty();
+            // Set a new debounce timer to delay the AJAX request
+            debounceTimer = setTimeout(function () {
+                $.ajax({
+                    type: 'GET',
+                    url: route,
+                    data: { searchTerm: searchTerm },
+                    success: function (data) {
+                        // Clear the card container
+                        $('#filteredCards').empty();
 
-                    // Check if searchTerm is empty
-                    if (searchTerm == "") {
-                        // Show the cardBoard and its children
-                        cardBoard.show();
-                    } else {
-                        // Hide the cardBoard and its children
-                        cardBoard.hide();
+                        // Check if searchTerm is empty
+                        if (searchTerm == "") {
+                            // Show the cardBoard and its children
+                            cardBoard.show();
+                        } else {
+                            // Hide the cardBoard and its children
+                            cardBoard.hide();
 
-                        // Access the 'modules' property directly from the response object
-                        data.modules.forEach(function (module) {
-                            var card = $('<div class="card"></div>');
-                            var cardContent = '<p class="center"><b>' + module.nom + '</b></p>' +
-                                              '<p><a href="' + module.deleteLink + '" class="looksLikeAButtonScarlet">Supprimer</a></p>' +
-                                              '<p>' + module.categorie + '</p>';
-                            
-                            card.html(cardContent);
-                            // Append more module details here if needed
-                            $('#filteredCards').append(card);
-                        });
+                            // Access the 'modules' property directly from the response object
+                            data.modules.forEach(function (module) {
+                                var card = $('<div class="card"></div>');
+                                var cardContent = '<p class="center"><b>' + module.nom + '</b></p>' +
+                                                '<p><a href="' + module.deleteLink + '" class="looksLikeAButtonScarlet">Supprimer</a></p>' +
+                                                '<p>' + module.categorie + '</p>';
+                                
+                                card.html(cardContent);
+                                // Append more module details here if needed
+                                $('#filteredCards').append(card);
+                            });
+                        }
+                    },
+                    error: function () {
+                        console.error('AJAX request failed.');
                     }
-                },
-                error: function () {
-                    console.error('AJAX request failed.');
-                }
-            });
-        }, 300); // Adjust the debounce time (in milliseconds) as needed
-    });
+                });
+            }, 300); // Adjust the debounce time (in milliseconds) as needed
+        });        
+    }
+    else
+    {
+
+    }
 });
